5b37b0fd53bbbbe12e0b23658571e4890bc80142
Shrey7966 <shre.learning.com>
2026-02-16 18:25:14 -0600

    Added a new program folder

--- DataStructures/Program01.java
+++ DataStructures/Program01.java
@@ -2,6 +2,6 @@ public class Program01
  {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to Data Structures!");
+
     }
 }
