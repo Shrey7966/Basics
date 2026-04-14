0f45f79c0f7bfcd91d8edbd7d9815b7ec3787301
Shreyas <shreyasg_96@outlook.com>
2026-04-14 18:58:31 -0500

    initial files

---
+++ src/test/java/com/quicklearn/demo/First/Spring/project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quicklearn.demo.First.Spring.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
